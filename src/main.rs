a48cd5637ab78a21f65762842068e4ac00703b65
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-11 11:14:57 +0000

    Array passing by value and reference

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use std::io;
 
+// For passing array as reference
+fn change_array(arr: &mut [i32;5]){
+	arr[0]=100;
+	println!("Array inside change_array function: {:?}", arr);
+}
+
+// For passing array as value
+// fn change_array(mut arr:[i32;5]){
+// 	arr[0]=100;
+// 	println!("Array inside change_array function: {:?}", arr);
+// }
+
 fn main(){
-	let arr=[1,2,3,4,5];
-	// for i in 0..5{
-	// 	println!("{}",arr[i]);
-	// }
-	for val in arr.iter(){
-		println!("{}",val);
-	}
+	// let arr=[1,2,3,4,5]; // For passing array as value
+	let mut arr=[1,2,3,4,5]; // For passing array as reference
+	println!("Array before change_array function: {:?}", arr);
+
+	// change_array(arr); // For passing array as value
+	change_array(&mut arr); // For passing array as reference
+
+
+	println!("Array after change_array function: {:?}", arr);
 }
