91ee5d30367c24a6228c8a00106eff5f7ef5f033
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-11 13:13:51 +0000

    Fibonacci in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::io;
 
+fn fibonacci(n:i32)->i32{
+    if n<=2 {
+        return 1;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
 fn main(){
-	let mut s=String::new();
+    let mut s=String::new();
     io::stdin().read_line(&mut s).expect("Failed to read line");
 
-    println!("{}", s); // For string input
+    let n:i32=s.trim().parse().expect("Failed to parse");
 
-    // let x:i32=s.trim().parse().expect("Failed to parse"); // For integer input
-    // println!("{}", x);
-
+    println!("The {}th fibonacci number is {}",n,fibonacci(n));
 }
