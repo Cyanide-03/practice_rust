d029e7b837b547369d5c7c1fcf4fdfc4665c4db5
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-11 13:26:53 +0000

    Ownership and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::io;
 
-fn fibonacci(n:i32)->i32{
-    if n<=2 {
-        return 1;
+fn count_vowels(s:&str)->i32{
+    let mut count=0;
+    for c in s.chars(){
+        // if c=='a' || c=='e' || c=='i' || c=='o' || c=='u'{
+        //     count+=1;
+        // }
+        if matches!(c,'a'|'e'|'i'|'o'|'u'|'A'|'E'|'I'|'O'|'U'){
+            count+=1;
+        }
     }
-    else{
-        return fibonacci(n-1)+fibonacci(n-2);
-    }
+    return count;
 }
 
 fn main(){
     let mut s=String::new();
+    println!("Enter a string:");
     io::stdin().read_line(&mut s).expect("Failed to read line");
-
-    let n:i32=s.trim().parse().expect("Failed to parse");
-
-    println!("The {}th fibonacci number is {}",n,fibonacci(n));
+    let trimmed=s.trim();
+    println!("The number of vowels in {} are {}",trimmed,count_vowels(trimmed));
 }
