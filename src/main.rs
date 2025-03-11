fb936de15545c07dc49a5f25f3c93bda04b742bf
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-11 13:00:38 +0000

    String and integer input in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
 use std::io;
 
-// For passing array as reference
-fn change_array(arr: &mut [i32;5]){
-	arr[0]=100;
-	println!("Array inside change_array function: {:?}", arr);
-}
-
-// For passing array as value
-// fn change_array(mut arr:[i32;5]){
-// 	arr[0]=100;
-// 	println!("Array inside change_array function: {:?}", arr);
-// }
-
 fn main(){
-	// let arr=[1,2,3,4,5]; // For passing array as value
-	let mut arr=[1,2,3,4,5]; // For passing array as reference
-	println!("Array before change_array function: {:?}", arr);
+	let mut s=String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read line");
 
-	// change_array(arr); // For passing array as value
-	change_array(&mut arr); // For passing array as reference
+    println!("{}", s); // For string input
 
+    // let x:i32=s.trim().parse().expect("Failed to parse"); // For integer input
+    // println!("{}", x);
 
-	println!("Array after change_array function: {:?}", arr);
 }
