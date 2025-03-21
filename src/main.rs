fd6959df6e1a065167bcc76bdf35436399e8b444
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-21 07:41:53 +0000

    Made a guessing game

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main(){
+		let secret_number=rand::thread_rng().gen_range(1..=100);
+
+		loop{
+				let mut guess=String::new();
+				println!("Enter a guess: ");
+				io::stdin()
+						.read_line(&mut guess)
+						.expect("Failed to read line");
+
+				let guess:i32=guess.trim().parse().expect("Please type a number");
+
+				match guess.cmp(&secret_number){
+						Ordering::Less=>println!("Too small"),
+						Ordering::Equal=>{
+								println!("You win");
+								break;
+						},
+						Ordering::Greater=>println!("Too large"),
+				}
+		}
+
+}
+
